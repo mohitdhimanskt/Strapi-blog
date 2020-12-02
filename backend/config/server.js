72f2517ff8332752855c6e9fd4f53357faeca86b
mohit <mohit.dhiman.sarkaghat@gmail.com>
2020-12-02 10:02:00 +0530

    Intial commit

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '2e180ad15d8e891a72bc18dedd2f1af7'),
+    },
+  },
+});
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '623ccf4c-77d2-4e2c-82a3-b77bc3453ee6'
+};
